a8d664523f7afae1d9353637d26650b86784eb4f
Aykhan Isgandarli <aykhan.isgandarli@azintelecom.az>
2023-01-23 14:00:22 +0400

    form

--- frontend/pages/admin/crud/blog/index.tsx
+++ frontend/pages/admin/crud/blog/index.tsx
@@ -1,8 +1,22 @@
+import InputComponent from 'packages/RInput/input.component'
 import React from 'react'
+import css from "./blog.module.scss"
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const BlogComponent = () => {
   return (
-    <div>BlogComponent</div>
+    <div className={css.blog_form}>
+      <div className={css.blog_form_title}>
+        <h1>Create Blog Form</h1>
+      </div>
+      <div className="container">
+      <form>
+        <InputComponent label='Title'/>
+        <ReactQuill theme="snow" value={""}  />
+      </form>
+      </div>
+    </div>
   )
 }
 
